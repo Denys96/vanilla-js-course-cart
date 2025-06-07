a0cc2e456fae575b1320f135728ef38e844a51da
denys96 <denisalexaner_96@hotmail.com>
2025-06-07 10:05:21 -0600

    se agregaron funcionalidades importates dentro del codigo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { getCourses } from './list_courses';
+import { addCourse, deleteCourse } from './cart';
 
 document.addEventListener('DOMContentLoaded',() => {
     getCourses();
+
+    
+    // agregar un evento de escucha para agregar un elemento al carrito
+    document.querySelector('#list-courses').addEventListener('click', addCourse);
+
+    //agregando un evento para eliminar un curso
+    document.querySelector('#list-cart').addEventListener('click', deleteCourse)
 }) 
+
